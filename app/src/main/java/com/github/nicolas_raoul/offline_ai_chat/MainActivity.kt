36a1636883640f8296a2a545f7226db857687127
Nicolas Raoul <nicolas.raoul@gmail.com>
2025-04-16 11:59:54 +0900

    Removed config UI

--- app/src/main/java/com/github/nicolas_raoul/offline_ai_chat/MainActivity.kt
+++ app/src/main/java/com/github/nicolas_raoul/offline_ai_chat/MainActivity.kt
@@ -19,7 +19,6 @@ package com.github.nicolas_raoul.offline_ai_chat
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
-import android.widget.CompoundButton
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -29,8 +28,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.ai.edge.aicore.GenerativeAIException
 import com.google.ai.edge.aicore.GenerativeModel
 import com.github.nicolas_raoul.offline_ai_chat.ContentAdapter
-import com.github.nicolas_raoul.offline_ai_chat.GenerationConfigDialog
-import com.github.nicolas_raoul.offline_ai_chat.GenerationConfigUtils
 import com.github.nicolas_raoul.offline_ai_chat.R
 import com.google.ai.edge.aicore.generationConfig
 import java.util.concurrent.Future
@@ -38,15 +35,12 @@ import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.future.future
 
 /** Demonstrates the AICore SDK usage from Kotlin. */
-class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateListener {
+class MainActivity : AppCompatActivity() {
 
   private var requestEditText: EditText? = null
   private var sendButton: Button? = null
-  private var streamingSwitch: CompoundButton? = null
-  private var configButton: Button? = null
   private var contentRecyclerView: RecyclerView? = null
   private var model: GenerativeModel? = null
-  private var useStreaming = false
   private var inGenerating = false
   private var generateContentFuture: Future<Unit>? = null
 
@@ -76,17 +70,6 @@ class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateL
       inGenerating = !inGenerating
     }
 
-    streamingSwitch = findViewById<CompoundButton>(R.id.streaming_switch)
-    streamingSwitch!!.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
-      useStreaming = isChecked
-    }
-    useStreaming = streamingSwitch!!.isChecked
-
-    configButton = findViewById(R.id.config_button)
-    configButton!!.setOnClickListener {
-      GenerationConfigDialog().show(supportFragmentManager, null)
-    }
-
     contentRecyclerView = findViewById<RecyclerView>(R.id.content_recycler_view)
     contentRecyclerView!!.layoutManager = LinearLayoutManager(this)
     contentRecyclerView!!.adapter = contentAdapter
@@ -104,9 +87,9 @@ class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateL
       GenerativeModel(
         generationConfig {
           context = applicationContext
-          temperature = GenerationConfigUtils.getTemperature(applicationContext)
-          topK = GenerationConfigUtils.getTopK(applicationContext)
-          maxOutputTokens = GenerationConfigUtils.getMaxOutputTokens(applicationContext)
+          temperature = 0.2f
+          topK = 16
+          maxOutputTokens = 256
         }
       )
   }
@@ -115,28 +98,22 @@ class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateL
     generateContentFuture =
       lifecycleScope.future {
         try {
-          if (useStreaming) {
-            var hasFirstStreamingResult = false
-            var result = ""
-            model!!
-              .generateContentStream(request)
-              .onCompletion { endGeneratingUi() }
-              .collect { response ->
-                run {
-                  result += response.text
-                  if (hasFirstStreamingResult) {
-                    contentAdapter.updateStreamingResponse(result)
-                  } else {
-                    contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE, result)
-                    hasFirstStreamingResult = true
-                  }
+          var hasFirstStreamingResult = false
+          var result = ""
+          model!!
+            .generateContentStream(request)
+            .onCompletion { endGeneratingUi() }
+            .collect { response ->
+              run {
+                result += response.text
+                if (hasFirstStreamingResult) {
+                  contentAdapter.updateStreamingResponse(result)
+                } else {
+                  contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE, result)
+                  hasFirstStreamingResult = true
                 }
               }
-          } else {
-            val response = model!!.generateContent(request)
-            contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE, response.text!!)
-            endGeneratingUi()
-          }
+            }
         } catch (e: GenerativeAIException) {
           contentAdapter.addContent(ContentAdapter.VIEW_TYPE_RESPONSE_ERROR, e.message!!)
           endGeneratingUi()
@@ -147,19 +124,10 @@ class MainActivity : AppCompatActivity(), GenerationConfigDialog.OnConfigUpdateL
   private fun startGeneratingUi() {
     sendButton?.setText(R.string.button_cancel)
     requestEditText?.setText(R.string.empty)
-    streamingSwitch?.isEnabled = false
-    configButton?.isEnabled = false
   }
 
   private fun endGeneratingUi() {
     sendButton?.setText(R.string.button_send)
-    streamingSwitch?.isEnabled = true
-    configButton?.isEnabled = true
     contentRecyclerView?.smoothScrollToPosition(contentAdapter.itemCount - 1)
-  }
-
-  override fun onConfigUpdated() {
-    model?.close()
-    initGenerativeModel()
   }
 }
